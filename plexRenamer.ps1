0a96443b822ba0358b73b781254ef219953bd79d
Daniel Gibson <dag@ellabs.ellabusa.com>
2023-04-18 12:46:41 -0600

    Condensed GUI

--- plexRenamer.ps1
+++ plexRenamer.ps1
@@ -1,52 +1,117 @@
 Add-Type -AssemblyName System.Windows.Forms
 Add-Type -AssemblyName System.Drawing
 
-#apiKey var here
+function Get-Gui {
+    param (
+        #
+    )
 
-
-function Get-Drive {
+    # Create form
     $form = New-Object System.Windows.Forms.Form
-    $form.Text = 'Plex Rename'
-    $form.Size = New-Object System.Drawing.Size(300,200)
-    $form.StartPosition = 'CenterScreen'
+    $form.Text = "TV Show Information"
+    $form.Size = New-Object System.Drawing.Size(450, 320)
+    $form.StartPosition = "CenterScreen"
+    $form.TopMost = $true
     
+    # Create API Key label and text box
+    $apiKeyLabel = New-Object System.Windows.Forms.Label
+    $apiKeyLabel.Location = New-Object System.Drawing.Point(20, 20)
+    $apiKeyLabel.Size = New-Object System.Drawing.Size(100, 23)
+    $apiKeyLabel.Text = "API Key:"
+    $form.Controls.Add($apiKeyLabel)
+    
+    $apiKeyTextBox = New-Object System.Windows.Forms.TextBox
+    $apiKeyTextBox.Location = New-Object System.Drawing.Point(120, 20)
+    $apiKeyTextBox.Size = New-Object System.Drawing.Size(250, 23)
+    $form.Controls.Add($apiKeyTextBox)
+    
+    # Create Drive name label and text box
+    $driveNameLabel = New-Object System.Windows.Forms.Label
+    $driveNameLabel.Location = New-Object System.Drawing.Point(20, 60)
+    $driveNameLabel.Size = New-Object System.Drawing.Size(100, 23)
+    $driveNameLabel.Text = "Drive Name:"
+    $form.Controls.Add($driveNameLabel)
+    
+    $driveNameTextBox = New-Object System.Windows.Forms.TextBox
+    $driveNameTextBox.Location = New-Object System.Drawing.Point(120, 60)
+    $driveNameTextBox.Size = New-Object System.Drawing.Size(250, 23)
+    $form.Controls.Add($driveNameTextBox)
+    
+    # Create dir name label and text box
+    $dirNameLabel = New-Object System.Windows.Forms.Label
+    $dirNameLabel.Location = New-Object System.Drawing.Point(20, 100)
+    $dirNameLabel.Size = New-Object System.Drawing.Size(100, 23)
+    $dirNameLabel.Text = "Media folder:"
+    $form.Controls.Add($dirNameLabel)
+    
+    $dirNameTextBox = New-Object System.Windows.Forms.TextBox
+    $dirNameTextBox.Location = New-Object System.Drawing.Point(120, 100)
+    $dirNameTextBox.Size = New-Object System.Drawing.Size(250, 23)
+    $form.Controls.Add($dirNameTextBox)
+        
+    
+    # Create Media name label and text box
+    $mediaNameLabel = New-Object System.Windows.Forms.Label
+    $mediaNameLabel.Location = New-Object System.Drawing.Point(20, 140)
+    $mediaNameLabel.Size = New-Object System.Drawing.Size(100, 23)
+    $mediaNameLabel.Text = "Media Name:"
+    $form.Controls.Add($mediaNameLabel)
+    
+    $mediaNameTextBox = New-Object System.Windows.Forms.TextBox
+    $mediaNameTextBox.Location = New-Object System.Drawing.Point(120, 140)
+    $mediaNameTextBox.Size = New-Object System.Drawing.Size(250, 23)
+    $form.Controls.Add($mediaNameTextBox)
+    
+    # Create Season label and text box
+    $seasonLabel = New-Object System.Windows.Forms.Label
+    $seasonLabel.Location = New-Object System.Drawing.Point(20, 180)
+    $seasonLabel.Size = New-Object System.Drawing.Size(100, 23)
+    $seasonLabel.Text = "Season:"
+    $form.Controls.Add($seasonLabel)
+    
+    $seasonTextBox = New-Object System.Windows.Forms.TextBox
+    $seasonTextBox.Location = New-Object System.Drawing.Point(120, 180)
+    $seasonTextBox.Size = New-Object System.Drawing.Size(250, 23)
+    $form.Controls.Add($seasonTextBox)
+    
+    # Create TV show checkbox
+    $tvShowCheckBox = New-Object System.Windows.Forms.CheckBox
+    $tvShowCheckBox.Location = New-Object System.Drawing.Point(20, 220)
+    $tvShowCheckBox.Size = New-Object System.Drawing.Size(150, 23)
+    $tvShowCheckBox.Text = "Is this a TV show?"
+    $form.Controls.Add($tvShowCheckBox)
+    
+    # Create OK button
     $okButton = New-Object System.Windows.Forms.Button
-    $okButton.Location = New-Object System.Drawing.Point(75,120)
-    $okButton.Size = New-Object System.Drawing.Size(75,23)
-    $okButton.Text = 'OK'
+    $okButton.Location = New-Object System.Drawing.Point(180, 240)
+    $okButton.Size = New-Object System.Drawing.Size(75, 23)
+    $okButton.Text = "OK"
     $okButton.DialogResult = [System.Windows.Forms.DialogResult]::OK
     $form.AcceptButton = $okButton
     $form.Controls.Add($okButton)
     
+    # Create Cancel button
     $cancelButton = New-Object System.Windows.Forms.Button
-    $cancelButton.Location = New-Object System.Drawing.Point(150,120)
-    $cancelButton.Size = New-Object System.Drawing.Size(75,23)
-    $cancelButton.Text = 'Cancel'
+    $cancelButton.Location = New-Object System.Drawing.Point(270, 240)
+    $cancelButton.Size = New-Object System.Drawing.Size(75, 23)
+    $cancelButton.Text = "Cancel"
     $cancelButton.DialogResult = [System.Windows.Forms.DialogResult]::Cancel
     $form.CancelButton = $cancelButton
     $form.Controls.Add($cancelButton)
     
-    $label = New-Object System.Windows.Forms.Label
-    $label.Location = New-Object System.Drawing.Point(10,20)
-    $label.Size = New-Object System.Drawing.Size(280,20)
-    $label.Text = 'Enter the drive of your choice:'
-    $form.Controls.Add($label)
+    # Show the form
+    $form.ShowDialog() | Out-Null
     
-    $textBox = New-Object System.Windows.Forms.TextBox
-    $textBox.Location = New-Object System.Drawing.Point(10,40)
-    $textBox.Size = New-Object System.Drawing.Size(260,20)
-    $form.Controls.Add($textBox)
+    # Retrieve input values
+    $apiKey = $apiKeyTextBox.Text
+    $driveName = $driveNameTextBox.Text+ ':'
+    $mediaDir = $dirNameTextBox.Text
+    $mediaName = $mediaNameTextBox.Text
+    $season = $seasonTextBox.Text
+    $isTVShow = $tvShowCheckBox.Checked
+
+    return $apiKey, $driveName, $mediaName, $season, $isTVShow, $mediaDir
     
-    $form.Topmost = $true
-    
-    $form.Add_Shown({$textBox.Select()})
-    $result = $form.ShowDialog()
-    
-    if ($result -eq [System.Windows.Forms.DialogResult]::OK)
-    {
-        $drive = $textBox.Text + ':'
-        return $drive
-    }
 }
 
 function Get-Dir {
@@ -63,107 +128,6 @@ function Get-Dir {
         mkdir "$drive\Transfer\Staging"
     }
 }
-
-
-function Get-Media {
-    $form = New-Object System.Windows.Forms.Form
-    $form.Text = 'Plex Rename'
-    $form.Size = New-Object System.Drawing.Size(300,200)
-    $form.StartPosition = 'CenterScreen'
-
-    $okButton = New-Object System.Windows.Forms.Button
-    $okButton.Location = New-Object System.Drawing.Point(75,120)
-    $okButton.Size = New-Object System.Drawing.Size(75,23)
-    $okButton.Text = 'OK'
-    $okButton.DialogResult = [System.Windows.Forms.DialogResult]::OK
-    $form.AcceptButton = $okButton
-    $form.Controls.Add($okButton)
-
-    $cancelButton = New-Object System.Windows.Forms.Button
-    $cancelButton.Location = New-Object System.Drawing.Point(150,120)
-    $cancelButton.Size = New-Object System.Drawing.Size(75,23)
-    $cancelButton.Text = 'Cancel'
-    $cancelButton.DialogResult = [System.Windows.Forms.DialogResult]::Cancel
-    $form.CancelButton = $cancelButton
-    $form.Controls.Add($cancelButton)
-
-    $label = New-Object System.Windows.Forms.Label
-    $label.Location = New-Object System.Drawing.Point(10,20)
-    $label.Size = New-Object System.Drawing.Size(280,20)
-    $label.Text = 'Enter the name of the media:'
-    $form.Controls.Add($label)
-
-    $textBox = New-Object System.Windows.Forms.TextBox
-    $textBox.Location = New-Object System.Drawing.Point(10,40)
-    $textBox.Size = New-Object System.Drawing.Size(260,20)
-    $form.Controls.Add($textBox)
-
-    $form.Topmost = $true
-
-    $form.Add_Shown({$textBox.Select()})
-    $result = $form.ShowDialog()
-
-    if ($result -eq [System.Windows.Forms.DialogResult]::OK)
-    {
-        $media = $textBox.Text
-        return $media
-    }
-}
-
-function Get-Type {
-    $wshell = New-Object -ComObject Wscript.Shell
-    $answer = $wshell.Popup("Is this a season of a TV show?",0,"Alert",64+4)
-    # Yes = 6
-    # No = 7
-    return $answer
-}
-
-function Get-Season {
-    $form = New-Object System.Windows.Forms.Form
-    $form.Text = 'Plex Rename'
-    $form.Size = New-Object System.Drawing.Size(300,200)
-    $form.StartPosition = 'CenterScreen'
-    
-    $okButton = New-Object System.Windows.Forms.Button
-    $okButton.Location = New-Object System.Drawing.Point(75,120)
-    $okButton.Size = New-Object System.Drawing.Size(75,23)
-    $okButton.Text = 'OK'
-    $okButton.DialogResult = [System.Windows.Forms.DialogResult]::OK
-    $form.AcceptButton = $okButton
-    $form.Controls.Add($okButton)
-    
-    $cancelButton = New-Object System.Windows.Forms.Button
-    $cancelButton.Location = New-Object System.Drawing.Point(150,120)
-    $cancelButton.Size = New-Object System.Drawing.Size(75,23)
-    $cancelButton.Text = 'Cancel'
-    $cancelButton.DialogResult = [System.Windows.Forms.DialogResult]::Cancel
-    $form.CancelButton = $cancelButton
-    $form.Controls.Add($cancelButton)
-    
-    $label = New-Object System.Windows.Forms.Label
-    $label.Location = New-Object System.Drawing.Point(10,20)
-    $label.Size = New-Object System.Drawing.Size(280,20)
-    $label.Text = 'Enter the season of the show:'
-    $form.Controls.Add($label)
-    
-    $textBox = New-Object System.Windows.Forms.TextBox
-    $textBox.Location = New-Object System.Drawing.Point(10,40)
-    $textBox.Size = New-Object System.Drawing.Size(260,20)
-    $form.Controls.Add($textBox)
-    
-    $form.Topmost = $true
-    
-    $form.Add_Shown({$textBox.Select()})
-    $result = $form.ShowDialog()
-    
-    if ($result -eq [System.Windows.Forms.DialogResult]::OK)
-    {
-        $season = $textBox.Text
-        return $season
-    }
-    
-}
-
 
 
 #TODO : Need to change rename from .txt to any ext
@@ -199,7 +163,7 @@ function Get-TVContentID {
     param (
         $media
     )
-    $apiKey = ""
+    $apiKey = "xxxx"
     $endpointUrl = "https://api.themoviedb.org/3/search/tv?api_key=$apiKey&query=$media"
     $responseJson = Invoke-RestMethod -Method Get -Uri $endpointUrl
     foreach ($result in $responseJson.results) {
@@ -213,7 +177,7 @@ function Get-MovieContentID {
     param (
         $media
     )
-    $apiKey = ""
+    $apiKey = "xxxx"
     $endpointUrl = "https://api.themoviedb.org/3/search/movie?api_key=$apiKey&query=$media"
     $responseJson = Invoke-RestMethod -Method Get -Uri $endpointUrl
     foreach ($result in $responseJson.results) {
@@ -230,7 +194,7 @@ function Get-TvContentName {
         $drive,
         $media
     )
-    $apiKey = ""
+    $apiKey = "xxx"
     $pattern = '[\\/:*?"<>|]'
     $replacement = ''
     $dir = "$drive\Transfer\Rename\$media\Season $season"
@@ -250,29 +214,48 @@ function Get-TvContentName {
 
 function Get-MovieContentName {
     param (
-        #hello
+        $contentid,
+        $drive,
+        $media
     )
-    
+    $apiKey = "xxx"
+    $endpointUrl = "https://api.themoviedb.org/3/movie/$contentid`?api_key=$apikey"
+    $responseJson = Invoke-RestMethod -Method Get -Uri $endpointUrl
+    $fullName = $responseJson.name
+    Rename-Item -Path "OG ITEM NAME PATH" -NewName $fullName
+    # return $fullName
 }
 
 
-# 1: Get the drive that will use this program, then see if the associated DIRs already exist or not. If not create the dirs
-$drive = Get-Drive
+
+
+# 1 Grab GUI an assign vars
+
+$values = Get-Gui
+$apiKey = $values[0]
+$drive = $values[1]
+$media = $values[2]
+$season = $values[3]
+$isTV = $values[4]
+$mediaDir = $values[5]
+
+# $apiKey, $driveName, $mediaName, $season, $isTVShow
+
+# 2: Check if drive assc dirs are there.
+
 Get-Dir($drive)
 
-# 2: After checking for the dirs, get the media content name and season of show
-# TODO: Need to check if movie or not. If a movie, then skip season UI prompt. If TV with season, loop to sequeintially name
+
+# 3: Rename sesaon sequentially and assign dir paths
+
 $dir = "$drive\Transfer\Rename"
 $dirName = Get-ChildItem $dir -Name
-$media = Get-Media
-$answer = Get-Type
 
 Rename-Item -Path "$dir\$dirName" -NewName $media
 $dirRename = "$drive\Transfer\Rename\$media"
 $files = Get-ChildItem -Path $dirRename -File
 
-if ($answer -eq 6) {
-    $season = Get-Season
+if ($isTV -eq 'True') {
     Rename-MediaSeason -season $season -media $media -drive $drive
 }
 else {
@@ -281,39 +264,44 @@ else {
 
 
 
-# 3: Assoisate media content with corrent naming through TMDB
-# TODO: create fn to link media to TMDD
+# 4: Assoisate media content with corrent naming through TMDB
+# TODO: create fn to link media to TMDB API
 #   - Show vs movie. Diff fn or in same one with logic sepperated. Need the loop for TV show 
 
-if ($answer -eq 6) {
+if ($isTV -eq 'True') {
     $contentID = Get-TVContentID($media)
     Get-TvContentName -contentid $contentID -season $season -drive $drive -media $media
 }
 else {
-    #Get-MovieContentID
+    $contentID = Get-MovieContentID
+    Write-Host $contentID
+    #Get-MovieContentName -contentid $contentID -drive $drive -media $media
 }
 
 
 
-# 4: Move new media content from Rename to Staging dir 
-# $dirStage = "D:\Testing\Transfer\Staging"
+
+# 5: Move new media content from Rename to Staging dir 
+$dirStage = "$drive\Transfer\Staging"
+# robocopy "$drive\Transfer\Rename" $dirStage /MOVE /E /COPYALL /DCOPY:T /XD "$drive\Transfer\Rename"
+# Remove-Item -Path "F:\Transfer\Rename\$media\*" -Recurse
+# Remove-Item "F:\Transfer\Rename\$media\*" -Recurse -Force -Exclude "F:\Transfer\Rename\$media"
+
 # Move-Item $dirRename -Destination $dirStage -Recurse
 
 
 
 
 
+# 5: Call plexMover
+# Needs to have flag for asking if u waant push?
 
 
 
 
-# Add Section for ext
+
 
 #Todo: Add sub dir for each seaason within name of content
-
-
-
-# Move files to staging
 
 
 
